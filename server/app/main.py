4df89ce92b6af561daa743d453752f179d13bcd1
kevinh81760 <kevinh81760@gmail.com>
2025-04-11 16:15:02 -0700

    database set up

--- server/app/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    from app.routes.main_routes import main
-    app.register_blueprint(main)
-
-    
--- server/run.py
+++ server/run.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+from config import Config
 from app.routes.main_routes import main
 
 app = Flask(__name__) 
+
+# Load all settings from Config class into app
+app.config.from_object(Config)
+
+# Creates a connecting handler 
+mysql = MySQL(app)
+# Attaches mySQL connection directly onto the Flask object 
+app.mysql = mysql
 
 app.register_blueprint(main)
 
